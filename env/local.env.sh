c4a7f5a229c83a1cc6ee5c229f897a1e1001e0bd
Jeffrey Keene <thealtered7@gmail.com>
2019-04-05 21:33:11 -0600

    ADD_SQL_ALCHEMY: flushing out some of the README

---
+++ env/local.env.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+PGHOST=localhost
+PGUSER=postgres
+PGPASSWORD=fart
+PGDATABASE=points
+PGPORT=5432
+
+
+export PGHOST
+export PGUSER
+export PGPASSWORD
+export PGDATABASE
+export PGPORT
+
